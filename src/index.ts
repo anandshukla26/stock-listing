10a481a79ed0d3f126206b4097103877dd410802
Shukla Anand <ts-anand.shukla@rakuten.com>
2021-10-13 17:04:21 +0900

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!')
+const express = require('express');
+
+const app = express(); 
+
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', ()=> {
+    console.log('Hit me');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
